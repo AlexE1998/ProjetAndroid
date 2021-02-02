06d56075dc383c823cf27de9ca1297cb98418ea8
AlexE1998 <aernault44@gmail.com>
2021-02-02 22:37:19 +0100

    last version

---
+++ app/src/main/java/com/example/zeproject/AppliActivity.java
@@ -0,0 +1,69 @@
+package com.example.zeproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class AppliActivity extends AppCompatActivity {
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appli);
+        mTextViewResult = findViewById(R.id.text_view_result);
+        mQueue = Volley.newRequestQueue(this);
+
+        jsonParse();
+    }
+
+    private void jsonParse() {
+        String url = "https://swapi.dev/api/people/";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("results");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject result = jsonArray.getJSONObject(i);
+                
+                                String name = result.getString("name");
+                                int height = result.getInt("height");
+                                String gender = result.getString("gender");
+
+                                mTextViewResult.append(name + ", " + String.valueOf(height) + "cm, "+ gender + "\n\n");
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+
+            }
+        });
+
+        mQueue.add(request);
+    }
+}
